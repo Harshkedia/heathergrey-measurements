6077312a00bcd807c879c8febca39ec409c612dc
suryansh <makhariasuryansh7@gmail.com>
2023-08-13 16:56:31 +0530

    change back to the website url

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,9 @@ export const Header = () => {
     <div className="w-screen bg-white h-[90px] flex items-center justify-between z-[999] shadow-sm px-[8rem]">
       <div className="flex items-center w-4/5 h-full">
         <div className="grow uppercase font-[500] cursor-pointer text-[15px]">
-          <a href={"https://heathergreycollective.com/"}>Back to the website</a>
+          <a href={"https://heathergreycollective.com/store"}>
+            Back to the website
+          </a>
         </div>
       </div>
       <div>
